51573744055a7aa3b53eb8f18d234784a4d48e68
Sergie Arizandieta <78749398+SergieArizandieta@users.noreply.github.com>
2023-06-16 18:51:46 -0600

    addding Asignación de memoria
    + consuming data of Asignación de memoria  of VM
    + conditions render

--- frontend/src/components/AdministradorTareas.jsx
+++ frontend/src/components/AdministradorTareas.jsx
@@ -1,49 +1,18 @@
 import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 
-function AdministradorTareas({AllProcesos,AllGenerales}) {
-  // const [AllProcesos, setAllProcesos] = useState([
-  //   {
-  //     "Pid": 1,
-  //     "Nombre": "systemd",
-  //     "Usuario": 0,
-  //     "Estado": 1,
-  //     "Ram": 12,
-  //     "Padre": 0
-  //   },
-  //   {
-  //     "Pid": 2,
-  //     "Nombre": "kthreadd",
-  //     "Usuario": 0,
-  //     "Estado": 1,
-  //     "Ram": 0,
-  //     "Padre": 1
-  //   },
-  //   {
-  //     "Pid": 3,
-  //     "Nombre": "rcu_gp",
-  //     "Usuario": 0,
-  //     "Estado": 1026,
-  //     "Ram": 0,
-  //     "Padre": 2
-  //   },
-  //   {
-  //     "Pid": 4,
-  //     "Nombre": "rcu_gp",
-  //     "Usuario": 0,
-  //     "Estado": 1026,
-  //     "Ram": 0,
-  //     "Padre": 0
-  //   },
-  // ]);
+import Paper from '@mui/material/Paper';
 
+function AdministradorTareas({ AllProcesos, AllGenerales }) {
+  const [pid, setPid] = useState(null); 
+  const [arrMemory, setArrMemory] = useState([]); 
   const [procesosDesplegados, setProcesosDesplegados] = useState({});
 
   const handleDesplegarProcesos = (pid) => {
     setProcesosDesplegados((prev) => ({ ...prev, [pid]: !prev[pid] }));
   };
 
-  const handleKill = async(pid) => {
+  const handleKill = async (pid) => {
     console.log(pid);
     try {
       const response = await fetch(`http://${process.env.REACT_APP_PUERTO}:8080/tasks`, {
@@ -53,9 +22,48 @@ function AdministradorTareas({AllProcesos,AllGenerales}) {
           'Content-Type': 'text/plain'
         }
       });
-  
+
       if (response.ok) {
         console.log('La solicitud POST fue exitosa');
+       
+        // Realizar acciones adicionales si la solicitud es exitosa
+      } else {
+        console.log('La solicitud POST falló');
+        // Realizar acciones adicionales si la solicitud falla
+      }
+    } catch (error) {
+      console.log('Error al realizar la solicitud POST:', error);
+      // Realizar acciones adicionales en caso de error
+    }
+  };
+  
+  const closeMemory = () => {
+    console.log("closeMemory");
+    setPid(null);
+  };
+
+  const handleSeeMemory = async (pid) => {
+    console.log(pid);
+    try {
+      const response = await fetch(`http://${process.env.REACT_APP_PUERTO}:8080/memory`, {
+        method: 'POST',
+        body: pid, // Cuerpo de la solicitud POST, asegúrate de que sea un número entero válido
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      });
+
+      if (response.ok) {
+        console.log('La solicitud POST fue exitosa');
+        // Recuperar la respuesta como JSON
+        const data = await response.json();
+        setPid(pid);
+
+        // Realizar acciones adicionales con la respuesta JSON
+        console.log("1//", data);
+        setArrMemory(data);
+
+
         // Realizar acciones adicionales si la solicitud es exitosa
       } else {
         console.log('La solicitud POST falló');
@@ -67,16 +75,15 @@ function AdministradorTareas({AllProcesos,AllGenerales}) {
     }
   };
 
-
   const crearFilas = (procesos) => {
     return procesos.map((proceso) => (
       <React.Fragment key={proceso.pid}>
         <tr className={procesosDesplegados[proceso.pid] && 'desplegado'}>
-          <td style={{backgroundColor: `${proceso.pid === 0 ||!AllProcesos.some((q) => q.pid === proceso.padre) ? "#FFE659": "" }`}} >{proceso.pid}</td>
+          <td style={{ backgroundColor: `${proceso.pid === 0 || !AllProcesos.some((q) => q.pid === proceso.padre) ? "#FFE659" : ""}` }} >{proceso.pid}</td>
           <td>{proceso.nombre}</td>
           <td>{proceso.usuario}</td>
           <td>{proceso.estado}</td>
-          <td>{(proceso.ram/(AllGenerales[AllGenerales.length-1].totalram-Math.floor(Math.random() * (600 - 500 + 1) + 500))*100).toFixed(2)}</td>
+          <td>{(proceso.ram / (AllGenerales[AllGenerales.length - 1].totalram - Math.floor(Math.random() * (600 - 500 + 1) + 500)) * 100).toFixed(2)}</td>
           <td>{proceso.padre}</td>
           <td>
             {AllProcesos.filter((p) => p.padre === proceso.pid).length > 0 && (
@@ -86,10 +93,16 @@ function AdministradorTareas({AllProcesos,AllGenerales}) {
             )}
           </td>
           <td>
-              <button onClick={() => handleKill(proceso.pid)}>
-               x 
-              </button>
-          
+            <button onClick={() => handleKill(proceso.pid)}>
+              x
+            </button>
+
+          </td>
+          <td>
+            <button onClick={() => handleSeeMemory(proceso.pid)}>
+              👀
+            </button>
+
           </td>
         </tr>
         {procesosDesplegados[proceso.pid] &&
@@ -97,28 +110,83 @@ function AdministradorTareas({AllProcesos,AllGenerales}) {
       </React.Fragment>
     ));
   };
-  
+
 
   return (
     <div>
       <table>
         <thead>
           <tr>
-            <th style={{width: 200}} ><Typography variant="h5" color="inherit" component="div"><b>Pid</b></Typography></th>
-            <th style={{width: 200}}><Typography variant="h5" color="inherit" component="div"><b>Nombre</b></Typography></th>
-            <th style={{width: 200}}><Typography variant="h5" color="inherit" component="div"><b>Usuario</b></Typography></th>
-            <th style={{width: 200}}><Typography variant="h5" color="inherit" component="div"><b>Estado</b></Typography></th>
-            <th style={{width: 200}}><Typography variant="h5" color="inherit" component="div"><b>Ram (%)</b></Typography></th>
-            <th style={{width: 200}}><Typography variant="h5" color="inherit" component="div"><b>Padre</b></Typography></th>
-            <th style={{width: 200}}><Typography variant="h5" color="inherit" component="div"><b>Show More</b></Typography></th>
-            <th style={{width: 200}}><Typography variant="h5" color="inherit" component="div"><b>Kill</b></Typography></th>
+            <th style={{ width: 200 }} ><Typography variant="h5" color="inherit" component="div"><b>Pid</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Nombre</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Usuario</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Estado</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Ram (%)</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Padre</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Show More</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Kill</b></Typography></th>
+            <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Ver Memoria</b></Typography></th>
           </tr>
         </thead>
         <tbody>
-          {crearFilas(AllProcesos.filter((p) => p.padre === 0 || !AllProcesos.some((q) => q.pid === p.padre)) )}
+          {crearFilas(AllProcesos.filter((p) => p.padre === 0 || !AllProcesos.some((q) => q.pid === p.padre)))}
         </tbody>
       </table>
 
+      <br />
+      <br />
+      {
+        pid !== null &&
+        <div>
+        <Paper >
+          <center>
+            <Typography variant="h4" color="inherit" component="div">
+              <button style={{marginRight:"10px",marginBottom:"10px"}}  onClick={() => closeMemory()}>
+                cerrar
+              </button>
+              Asignación de memoria del proceso - PID: {pid}
+              
+            </Typography>
+            
+          </center>
+        </Paper>
+        <table>
+          <thead>
+            <tr>
+              <th style={{ width: 200 }} ><Typography variant="h5" color="inherit" component="div"><b>Dispositivo</b></Typography></th>
+              <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Archivo</b></Typography></th>
+              <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Direccion Inicial</b></Typography></th>
+              <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Direccion Final</b></Typography></th>
+              <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Permisos</b></Typography></th>
+              <th style={{ width: 200 }}><Typography variant="h5" color="inherit" component="div"><b>Tamaño (KB)</b></Typography></th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            arrMemory.map((memoria,index) => (
+              <tr key={index}>
+                <td style={{ backgroundColor:  `${index % 2 === 0 ? "#FFEE8C" : "#D8D8D8"}`}} >{memoria.device}</td>
+                <td style={{ backgroundColor:  `${index % 2 === 0 ? "#FFEE8C" : "#D8D8D8"}`}} >{memoria.file}</td>
+                <td style={{ backgroundColor: `${index % 2 === 0 ? "#FFEE8C" : "#D8D8D8"}`}} >{memoria.initial_address}</td>
+                <td style={{ backgroundColor:  `${index % 2 === 0 ? "#FFEE8C" : "#D8D8D8"}`}} >{memoria.final_address}</td>
+                <td style={{ backgroundColor:  `${index % 2 === 0 ? "#FFEE8C" : "#D8D8D8"}`}} >{memoria.permissions.join(" - ")}</td>
+                <td style={{ backgroundColor:  `${index % 2 === 0 ? "#FFEE8C" : "#D8D8D8"}`}} >{memoria.size_kb}</td>
+              </tr>
+            ))
+
+          }
+          {/* <tr>
+            <td style={{ backgroundColor:  "#FFE659"}} >{1}</td>
+            <td style={{ backgroundColor:  "#FFE659"}} >{1}</td>
+            <td style={{ backgroundColor:  "#FFE659"}} >{1}</td>
+            <td style={{ backgroundColor:  "#FFE659"}} >{1}</td>
+            <td style={{ backgroundColor:  "#FFE659"}} >{1}</td>
+            <td style={{ backgroundColor:  "#FFE659"}} >{1}</td>
+          </tr> */}
+          </tbody>
+        </table>
+      </div>
+      }
     </div>
   );
 }
